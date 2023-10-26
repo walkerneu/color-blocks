65f48311e3dec6fc3d67c45149e91ca70e338b6d
Walker Neudorff <walkerneudorff@gmail.com>
2023-10-26 16:35:00 -0500

    Finished counter objective

--- script.js
+++ script.js
@@ -4,46 +4,58 @@ function redButtonAdd() {
     console.log('You clicked red button!');
     document.getElementById('buttons');
     buttons.innerHTML += `<div onclick='redButtonDelete(event)' class="block red-fill"></div>`;
-  
+    
+    let redcount = Number(document.getElementById("red-count").textContent)
+    redcount ++
+    document.getElementById("red-count").textContent = redcount;
   }
 
-  function redButtonDelete (event) {
-    console.log('delete red!', event.target);
-    event.target.remove();
-  }
+//   function redButtonDelete (event) {
+//     console.log('delete red!', event.target);
+//     event.target.remove();
+//   }
 
   function blueButtonAdd() {
     console.log('You clicked blue button!');
     document.getElementById('buttons');
     buttons.innerHTML += `<div onclick='blueButtonDelete(event)' class="block blue-fill"></div>`;
   
+    let bluecount = Number(document.getElementById("blue-count").textContent)
+    bluecount ++
+    document.getElementById("blue-count").textContent = bluecount;
   }
 
-  function blueButtonDelete (event) {
-    console.log('delete blue!', event.target);
-    event.target.remove();
-  }
+//   function blueButtonDelete (event) {
+//     console.log('delete blue!', event.target);
+//     event.target.remove();
+//   }
 
   function greenButtonAdd() {
     console.log('You clicked green button!');
     document.getElementById('buttons');
     buttons.innerHTML += `<div onclick='greenButtonDelete(event)' class="block green-fill"></div>`;
   
+    let greencount = Number(document.getElementById("green-count").textContent)
+    greencount ++
+    document.getElementById("green-count").textContent = greencount;
   }
 
-  function greenButtonDelete (event) {
-    console.log('delete green!', event.target);
-    event.target.remove();
-  }
+//   function greenButtonDelete (event) {
+//     console.log('delete green!', event.target);
+//     event.target.remove();
+//   }
   
   function yellowButtonAdd() {
     console.log('You clicked yellow button!');
     document.getElementById('buttons');
     buttons.innerHTML += `<div onclick='yellowButtonDelete(event)' class="block yellow-fill"></div>`;
   
+    let yellowcount = Number(document.getElementById("yellow-count").textContent)
+    yellowcount ++
+    document.getElementById("yellow-count").textContent = yellowcount;
   }
 
-  function yellowButtonDelete (event) {
-    console.log('delete yellow!', event.target);
-    event.target.remove();
-  }
+//   function yellowButtonDelete (event) {
+//     console.log('delete yellow!', event.target);
+//     event.target.remove();
+//   }
